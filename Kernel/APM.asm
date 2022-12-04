d4c0dc7cca289fee0a26f8bf4b1e856ec4d5bc2c
leo007er1 <leonardofiorotto1@gmail.com>
2022-12-04 21:49:41 +0100

    Fix for APM
    Fixed APM with version 1.0 not allowing the kernel to continue executing

--- Kernel/APM.asm
+++ Kernel/APM.asm
@@ -26,36 +26,42 @@ ApmInit:
 
     ; If APM version is 1.1+ we are all good
     cmp al, byte 1
-    jl .OldVersion
+    jnl .ConnectInterface
+    call .OldVersion
 
-    mov byte [ApmMinorVersion], al
+    .ConnectInterface:
+        mov byte [ApmMinorVersion], al
 
-    ; Connect to Real Mode interface
-    mov ah, byte 0x53
-    mov al, byte 1
-    xor bx, bx
-    int 0x15
-    jc .ApmError
+        ; Connect to Real Mode interface
+        mov ah, byte 0x53
+        mov al, byte 1
+        xor bx, bx
+        int 0x15
+        jnc .GetDriverVer
+        call .ApmError
 
-    ; Get driver version
-    ; We want APM 1.1+ since with version 1.0 we can't shutdown the computer
-    mov ah, byte 0x53
-    mov al, byte 0xe
-    xor bx, bx
-    mov ch, byte 1 ; Major driver version in BCD format
-    mov cl, byte 1 ; Minor driver version in BCD format
-    int 0x15
-    jc .ApmError
+    .GetDriverVer:
+        ; Get driver version
+        ; We want APM 1.1+ since with version 1.0 we can't shutdown the computer
+        mov ah, byte 0x53
+        mov al, byte 0xe
+        xor bx, bx
+        mov ch, byte 1 ; Major driver version in BCD format
+        mov cl, byte 1 ; Minor driver version in BCD format
+        int 0x15
+        jnc .EnablePower
+        call .ApmError
 
-    ; Enables APM power management for all devices
-    mov ah, byte 0x53
-    mov al, byte 0x8
-    mov bx, 1 ; All devices
-    mov cx, 1 ; State, 1 = on
-    int 0x15
-    jc .ApmError
+    .EnablePower:
+        ; Enables APM power management for all devices
+        mov ah, byte 0x53
+        mov al, byte 0x8
+        mov bx, 1 ; All devices
+        mov cx, 1 ; State, 1 = on
+        int 0x15
+        jc .ApmError
 
-    ret
+        ret
 
 
     .OldVersion:
@@ -66,16 +72,20 @@ ApmInit:
         ret
 
     .ApmError:
-        lea si, ApmOldVerMessage
+        lea si, ApmGeneralError
         mov al, byte 0xc ; Red
         call VgaPrintString
 
-        cli
-        hlt
+        mov byte [ApmErrorByte], 1
+
+        ret
 
 
 ; Sets the power state of all devices to off
 ApmSystemShutdown:
+    cmp byte [ApmErrorByte], byte 1
+    jge .NoShutdown
+
     cmp byte [ApmMinorVersion], byte 1
     jl .NoShutdown
 
@@ -104,5 +114,6 @@ ApmSystemShutdown:
 
 ApmGeneralError: db 10, 13, "APM error while running ApmInit", 0
 ApmOldVerMessage: db "Old APM version: v1.0", 0
-ApmShutdownError: db 10, 13, "Can't shutdown computer: APM version 1.0", 0
+ApmShutdownError: db 10, 13, "Can't shutdown computer: APM version 1.0 or ApmInit error", 0
+ApmErrorByte: db 0
 ApmMinorVersion: db 0
